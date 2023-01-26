c95d009856e9a5b76f177f1b85800661117ee708
Sanket Patel <sanket.devops@gmail.com>
2023-01-26 12:33:07 -0500

    Sanket: update ssh module

--- ssh.py
+++ ssh.py
@@ -1,24 +1,23 @@
 import paramiko
 import sys
 
-hostName = "localhost"
-hostUser = "****"
-hostPass = "****"
-
-command = ["cat /etc/hostname", "ip add | grep '192.168'", "cat /etc/os-release"]
-# results = []
-
 def ssh_conn():
+    hostName = "192.168.120.191"
+    hostUser = "prod-proxy"
+    hostPass = "Yxorp$11-Jan-2023%dorP"
     results = []
+
+    commands = ["cat /etc/hostname", "ip add | grep '192.168'", "cat /home/prod-proxy/temp.txt"]
     client = paramiko.SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(hostName, username=hostUser, password=hostPass)
-    for x in command:
+    for command in commands:
         temp = []
-        ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command(x)
-        print(ssh_stdout)
+        ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command(command)
         for line in ssh_stdout:
-            temp.append(line.strip('\n'))
+            nRemove = line.strip('\n')
+            temp.append(nRemove)
         results.append(temp)
+    print(results)
     return results
